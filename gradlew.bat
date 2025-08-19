eae089c1bd94b948df23d9b4c86b8804bc147956
pavantangade59-byte <pavantangade59@gmail.com>
2025-08-19 15:40:04 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,2 +1,7 @@
 @echo off
-call gradle\wrapper\gradle-wrapper.bat %*
+setlocal
+
+set DIR=%~dp0
+set JAVA_EXE=java
+
+"%JAVA_EXE%" -Dorg.gradle.appname=%~n0 -classpath "%DIR%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
